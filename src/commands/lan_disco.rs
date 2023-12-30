9f568aea49bf237c556c759bcd3af3f86b49609c
Wez Furlong <wez@wezfurlong.org>
2023-12-29 19:06:10 -0700

    add more options for lan discovery

--- src/commands/lan_disco.rs
+++ src/commands/lan_disco.rs
@@ -1,11 +1,37 @@
-use crate::lan_api::Client;
+use crate::lan_api::{Client, DiscoOptions};
+use std::net::IpAddr;
 
 #[derive(clap::Parser, Debug)]
-pub struct LanDiscoCommand {}
+pub struct LanDiscoCommand {
+    /// Prevent the use of the default multicast broadcast address
+    #[arg(long)]
+    pub no_multicast: bool,
+
+    /// Enumerate all interfaces, and for each one that has
+    /// a broadcast address, broadcast to it
+    #[arg(long)]
+    pub broadcast_all: bool,
+
+    /// Broadcast to the global broadcast address 255.255.255.255
+    #[arg(long)]
+    pub global_broadcast: bool,
+
+    /// Addresses to scan. May be broadcast addresses or individual
+    /// IP addresses
+    #[arg(long)]
+    pub scan: Vec<IpAddr>,
+}
 
 impl LanDiscoCommand {
     pub async fn run(&self, args: &crate::Args) -> anyhow::Result<()> {
-        let (client, mut scan) = Client::new().await?;
+        let options = DiscoOptions {
+            enable_multicast: !self.no_multicast,
+            additional_addresses: self.scan.clone(),
+            broadcast_all_interfaces: self.broadcast_all,
+            global_broadcast: self.global_broadcast,
+        };
+
+        let (client, mut scan) = Client::new(options).await?;
 
         while let Some(device) = scan.recv().await {
             log::info!("{device:?}");
--- src/lan_api.rs
+++ src/lan_api.rs
@@ -1,6 +1,7 @@
 use anyhow::Context;
+use if_addrs::IfAddr;
 use serde::{Deserialize, Serialize};
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr};
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::net::UdpSocket;
@@ -10,9 +11,39 @@ use tokio::time::Instant;
 
 // <https://app-h5.govee.com/user-manual/wlan-guide>
 
+/// The port on which govee devices listen for scan requests
 const SCAN_PORT: u16 = 4001;
+/// The port on which a client needs to listen to receive responses
+/// from govee devices
 const LISTEN_PORT: u16 = 4002;
+/// The port on which govee devices listen for control requests
 const CMD_PORT: u16 = 4003;
+/// The multicast group of which govee LAN-API enabled devices are members
+const MULTICAST: IpAddr = IpAddr::V4(Ipv4Addr::new(239, 255, 255, 250));
+
+pub struct DiscoOptions {
+    /// Use the MULTICAST address defined in the LAN protocol
+    pub enable_multicast: bool,
+    /// Send to this list of additional addresses, which may
+    /// include multicast addresses
+    pub additional_addresses: Vec<IpAddr>,
+    /// Enumerate all interfaces, and for each one that has
+    /// a broadcast address, broadcast to it
+    pub broadcast_all_interfaces: bool,
+    /// Broadcast to the global broadcast address
+    pub global_broadcast: bool,
+}
+
+impl Default for DiscoOptions {
+    fn default() -> Self {
+        Self {
+            enable_multicast: true,
+            additional_addresses: vec![],
+            broadcast_all_interfaces: false,
+            global_broadcast: false,
+        }
+    }
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(tag = "cmd", content = "data")]
@@ -29,7 +60,7 @@ struct RequestMessage {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct ScannedDevice {
+pub struct LanDevice {
     pub ip: IpAddr,
     pub device: String,
     pub sku: String,
@@ -60,22 +91,11 @@ pub struct DeviceColor {
     pub b: u8,
 }
 
-#[derive(Debug)]
-pub struct LanDevice {
-    info: ScannedDevice,
-    addr: SocketAddr,
-}
-
 impl LanDevice {
-    pub fn with_scan(info: ScannedDevice, addr: IpAddr) -> Self {
-        let addr = SocketAddr::from((addr, CMD_PORT));
-        Self { info, addr }
-    }
-
     pub async fn send_request(&self, msg: Request) -> anyhow::Result<()> {
-        let client = UdpSocket::bind("0.0.0.0:0").await?;
+        let client = udp_socket_for_target(self.ip).await?;
         let data = serde_json::to_string(&RequestMessage { msg })?;
-        client.send_to(data.as_bytes(), self.addr).await?;
+        client.send_to(data.as_bytes(), (self.ip, CMD_PORT)).await?;
 
         Ok(())
     }
@@ -85,7 +105,7 @@ impl LanDevice {
 #[serde(tag = "cmd", content = "data")]
 pub enum Response {
     #[serde(rename = "scan")]
-    Scan(ScannedDevice),
+    Scan(LanDevice),
     #[serde(rename = "devStatus")]
     DevStatus(DeviceStatus),
 }
@@ -118,26 +138,110 @@ pub struct Client {
     inner: Arc<ClientInner>,
 }
 
-async fn lan_disco(inner: Arc<ClientInner>) -> anyhow::Result<Receiver<LanDevice>> {
-    let mcast = UdpSocket::bind("0.0.0.0:0").await?;
-    mcast.set_multicast_loop_v4(false)?;
-    mcast.join_multicast_v4(Ipv4Addr::new(239, 255, 255, 250), Ipv4Addr::UNSPECIFIED)?;
+#[derive(Debug)]
+struct Broadcaster {
+    addr: IpAddr,
+    socket: UdpSocket,
+}
 
-    let listen = UdpSocket::bind(("0.0.0.0", LISTEN_PORT)).await?;
-    let (tx, rx) = channel(8);
+async fn udp_socket_for_target(addr: IpAddr) -> std::io::Result<UdpSocket> {
+    match addr {
+        IpAddr::V4(_) => UdpSocket::bind((Ipv4Addr::UNSPECIFIED, 0)).await,
+        IpAddr::V6(_) => UdpSocket::bind((Ipv6Addr::UNSPECIFIED, 0)).await,
+    }
+}
 
-    async fn send_scan(mcast: &UdpSocket) -> anyhow::Result<()> {
-        let scan = serde_json::to_string(&RequestMessage {
-            msg: Request::Scan {
-                account_topic: AccountTopic::Reserve,
-            },
-        })
-        .expect("to serialize scan message");
-        mcast
-            .send_to(scan.as_bytes(), ("239.255.255.250", SCAN_PORT))
+impl Broadcaster {
+    pub async fn new(addr: IpAddr) -> std::io::Result<Self> {
+        let socket = udp_socket_for_target(addr).await?;
+
+        if addr.is_multicast() {
+            match addr {
+                IpAddr::V4(v4) => {
+                    socket.join_multicast_v4(v4, Ipv4Addr::UNSPECIFIED)?;
+                    socket.set_multicast_loop_v4(false)?;
+                }
+                IpAddr::V6(v6) => {
+                    socket.join_multicast_v6(&v6, 0)?;
+                    socket.set_multicast_loop_v6(false)?;
+                }
+            }
+        } else {
+            socket.set_broadcast(true)?;
+        }
+
+        Ok(Self { addr, socket })
+    }
+
+    pub async fn broadcast<B: AsRef<[u8]>>(&self, bytes: B) -> std::io::Result<()> {
+        self.socket
+            .send_to(bytes.as_ref(), (self.addr, SCAN_PORT))
             .await?;
         Ok(())
     }
+}
+
+async fn send_scan(options: &DiscoOptions) -> anyhow::Result<()> {
+    let mut addresses = options.additional_addresses.clone();
+    if options.enable_multicast {
+        addresses.push(MULTICAST);
+    }
+    if options.global_broadcast {
+        addresses.push(Ipv4Addr::BROADCAST.into());
+    }
+    if options.broadcast_all_interfaces {
+        match if_addrs::get_if_addrs() {
+            Ok(ifaces) => {
+                for iface in ifaces {
+                    if iface.is_loopback() {
+                        continue;
+                    }
+                    let bcast = match iface.addr {
+                        IfAddr::V4(v4) => v4.broadcast.map(IpAddr::V4),
+                        IfAddr::V6(v6) => v6.broadcast.map(IpAddr::V6),
+                    };
+                    if let Some(addr) = bcast {
+                        log::debug!("Adding bcast {addr} from if {}", iface.name);
+                        addresses.push(addr);
+                    }
+                }
+            }
+            Err(err) => {
+                log::error!("get_if_addrs: {err:#}");
+            }
+        }
+    }
+
+    let mut broadcasters = vec![];
+    for addr in addresses {
+        match Broadcaster::new(addr).await {
+            Ok(b) => broadcasters.push(b),
+            Err(err) => {
+                log::error!("{addr}: {err:#}");
+            }
+        }
+    }
+
+    let scan = serde_json::to_string(&RequestMessage {
+        msg: Request::Scan {
+            account_topic: AccountTopic::Reserve,
+        },
+    })
+    .expect("to serialize scan message");
+    for b in broadcasters {
+        if let Err(err) = b.broadcast(&scan).await {
+            log::error!("Error broadcasting to {b:?}: {err:#}");
+        }
+    }
+    Ok(())
+}
+
+async fn lan_disco(
+    options: DiscoOptions,
+    inner: Arc<ClientInner>,
+) -> anyhow::Result<Receiver<LanDevice>> {
+    let listen = UdpSocket::bind(("0.0.0.0", LISTEN_PORT)).await?;
+    let (tx, rx) = channel(8);
 
     async fn process_packet(
         addr: SocketAddr,
@@ -157,19 +261,19 @@ async fn lan_disco(inner: Arc<ClientInner>) -> anyhow::Result<Receiver<LanDevice
         }
 
         if let Response::Scan(info) = response.msg {
-            tx.send(LanDevice::with_scan(info, addr.ip())).await?;
+            tx.send(info).await?;
         }
 
         Ok(())
     }
 
     async fn run_disco(
-        mcast: UdpSocket,
+        options: &DiscoOptions,
         listen: UdpSocket,
         tx: Sender<LanDevice>,
         inner: Arc<ClientInner>,
     ) -> anyhow::Result<()> {
-        send_scan(&mcast).await?;
+        send_scan(options).await?;
 
         let mut retry_interval = Duration::from_secs(2);
         let max_retry = Duration::from_secs(60);
@@ -188,7 +292,7 @@ async fn lan_disco(inner: Arc<ClientInner>) -> anyhow::Result<Receiver<LanDevice
                     log::error!("recv_from: {err:#}");
                 }
                 Err(_) => {
-                    send_scan(&mcast).await?;
+                    send_scan(options).await?;
                     last_send = Instant::now();
                     retry_interval = (retry_interval * 2).min(max_retry);
                 }
@@ -197,7 +301,7 @@ async fn lan_disco(inner: Arc<ClientInner>) -> anyhow::Result<Receiver<LanDevice
     }
 
     tokio::spawn(async move {
-        if let Err(err) = run_disco(mcast, listen, tx, inner).await {
+        if let Err(err) = run_disco(&options, listen, tx, inner).await {
             log::error!("Error at the disco: {err:#}");
         }
     });
@@ -206,9 +310,9 @@ async fn lan_disco(inner: Arc<ClientInner>) -> anyhow::Result<Receiver<LanDevice
 }
 
 impl Client {
-    pub async fn new() -> anyhow::Result<(Self, Receiver<LanDevice>)> {
+    pub async fn new(options: DiscoOptions) -> anyhow::Result<(Self, Receiver<LanDevice>)> {
         let inner = Arc::new(ClientInner::default());
-        let rx = lan_disco(Arc::clone(&inner)).await?;
+        let rx = lan_disco(options, Arc::clone(&inner)).await?;
 
         Ok((Self { inner }, rx))
     }
@@ -227,18 +331,20 @@ impl Client {
     /// receiver.
     pub async fn scan_ip(&self, addr: IpAddr) -> anyhow::Result<LanDevice> {
         let mut rx = self.add_listener(addr).await?;
-        let client = UdpSocket::bind("0.0.0.0:0").await?;
+
+        let bcast = Broadcaster::new(addr).await?;
         let scan = serde_json::to_string(&RequestMessage {
             msg: Request::Scan {
                 account_topic: AccountTopic::Reserve,
             },
         })
         .expect("to serialize scan message");
-        client.send_to(scan.as_bytes(), (addr, SCAN_PORT)).await?;
+        bcast.broadcast(scan).await?;
+
         loop {
             match tokio::time::timeout(Duration::from_secs(10), rx.recv()).await {
                 Ok(Some(Response::Scan(info))) => {
-                    return Ok(LanDevice::with_scan(info, addr));
+                    return Ok(info);
                 }
                 Ok(Some(_)) => {}
                 Ok(None) => anyhow::bail!("listener thread terminated"),
@@ -248,7 +354,7 @@ impl Client {
     }
 
     pub async fn query_status(&self, device: &LanDevice) -> anyhow::Result<DeviceStatus> {
-        let mut rx = self.add_listener(device.addr.ip()).await?;
+        let mut rx = self.add_listener(device.ip).await?;
         device.send_request(Request::DevStatus {}).await?;
         loop {
             match tokio::time::timeout(Duration::from_secs(10), rx.recv()).await {
