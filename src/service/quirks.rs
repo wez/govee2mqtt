9f80991f0cc580f4ac71d6063ac89f6268009411
Wez Furlong <wez@wezfurlong.org>
2024-01-06 17:13:52 -0700

    Add quirk for H6141
    refs: https://github.com/wez/govee2mqtt/issues/15

--- src/service/quirks.rs
+++ src/service/quirks.rs
@@ -7,17 +7,30 @@ pub struct Quirk {
     pub avoid_platform_api: bool,
 }
 
-static QUIRKS: &[Quirk] = &[Quirk {
-    sku: "H610A",
-    supports_rgb: true,
-    supports_brightness: true,
-    color_temp_range: Some((2000, 9000)),
-    // At the time of writing, the metadata
-    // returned by Govee is completely bogus for this
-    // device
-    // <https://github.com/wez/govee2mqtt/issues/7>
-    avoid_platform_api: true,
-}];
+static QUIRKS: &[Quirk] = &[
+    Quirk {
+        sku: "H610A",
+        supports_rgb: true,
+        supports_brightness: true,
+        color_temp_range: Some((2000, 9000)),
+        // At the time of writing, the metadata
+        // returned by Govee is completely bogus for this
+        // device
+        // <https://github.com/wez/govee2mqtt/issues/7>
+        avoid_platform_api: true,
+    },
+    Quirk {
+        sku: "H6141",
+        supports_rgb: true,
+        supports_brightness: true,
+        color_temp_range: Some((2000, 9000)),
+        // At the time of writing, the metadata
+        // returned by Govee is completely bogus for this
+        // device
+        // <https://github.com/wez/govee2mqtt/issues/15>
+        avoid_platform_api: true,
+    },
+];
 
 pub fn resolve_quirk(sku: &str) -> Option<&'static Quirk> {
     QUIRKS.iter().find(|q| q.sku == sku)
