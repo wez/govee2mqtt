9bbb2d952ca932aa93ff14fb7203ad1053c76d68
Wez Furlong <wez@wezfurlong.org>
2024-01-12 07:46:20 -0700

    automate changelog creation for the addon
    refs: https://github.com/wez/govee2mqtt/issues/52

--- scripts/tag-release.sh
+++ scripts/tag-release.sh
@@ -4,5 +4,6 @@
 TAG_NAME=${TAG_NAME:-$(git -c "core.abbrev=8" show -s "--format=%cd-%h" "--date=format:%Y.%m.%d")}
 git tag $TAG_NAME
 ./scripts/apply-tag.sh
+docker run -t -v "$(pwd)":/app/ "ghcr.io/orhun/git-cliff/git-cliff:${TAG:-latest}" -o addon/CHANGELOG.md -c scripts/cliff.toml
 git add addon/config.yaml
 git commit -m "Tag $TAG_NAME"
