362338bee9654f7dbbb5d2e23240f854ef5988e4
Wez Furlong <wez@wezfurlong.org>
2024-01-14 14:03:37 -0700

    Add skeleton for old rest api
    This is also write-only for appliances, and has less information
    available than the platform API.

--- src/rest_api.rs
+++ src/rest_api.rs
@@ -2,6 +2,7 @@ use crate::cache::{cache_get, CacheComputeResult, CacheGetOptions};
 use crate::platform_api::{http_response_body, ONE_WEEK};
 use reqwest::Method;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use serde_json::{json, Value as JsonValue};
 use tokio::time::Duration;
 
 // This file im0plements the older Govee REST API as described in:
@@ -18,6 +19,7 @@ pub struct RestApiClient {
     key: String,
 }
 
+#[allow(unused)]
 impl RestApiClient {
     pub fn new<K: Into<String>>(key: K) -> Self {
         Self { key: key.into() }
@@ -42,6 +44,100 @@ impl RestApiClient {
         .await
     }
 
+    pub async fn control_turn(&self, device: &RestDeviceInfo, on: bool) -> anyhow::Result<()> {
+        let request = json!({
+            "device": device.device,
+            "model": device.sku,
+            "cmd":{
+                "name": "turn",
+                "value": if on {"on"} else {"off"},
+            }
+        });
+
+        let resp: JsonValue = self
+            .request_with_json_response(Method::PUT, endpoint("/v1/devices/control"), &request)
+            .await?;
+
+        log::trace!("turn response: {resp:?}");
+
+        Ok(())
+    }
+
+    pub async fn control_brightness(
+        &self,
+        device: &RestDeviceInfo,
+        percent: u8,
+    ) -> anyhow::Result<()> {
+        let request = json!({
+            "device": device.device,
+            "model": device.sku,
+            "cmd":{
+                "name": "brightness",
+                "value": percent,
+            }
+        });
+
+        let resp: JsonValue = self
+            .request_with_json_response(Method::PUT, endpoint("/v1/devices/control"), &request)
+            .await?;
+
+        log::trace!("brightness response: {resp:?}");
+
+        Ok(())
+    }
+
+    pub async fn control_color(
+        &self,
+        device: &RestDeviceInfo,
+        r: u8,
+        g: u8,
+        b: u8,
+    ) -> anyhow::Result<()> {
+        let request = json!({
+            "device": device.device,
+            "model": device.sku,
+            "cmd":{
+                "name": "color",
+                "value": {
+                    "r":r,
+                    "g":g,
+                    "b":b,
+                },
+            }
+        });
+
+        let resp: JsonValue = self
+            .request_with_json_response(Method::PUT, endpoint("/v1/devices/control"), &request)
+            .await?;
+
+        log::trace!("color response: {resp:?}");
+
+        Ok(())
+    }
+
+    pub async fn control_color_temperature(
+        &self,
+        device: &RestDeviceInfo,
+        kelvin: u16,
+    ) -> anyhow::Result<()> {
+        let request = json!({
+            "device": device.device,
+            "model": device.sku,
+            "cmd":{
+                "name": "colorTem",
+                "value": kelvin,
+            }
+        });
+
+        let resp: JsonValue = self
+            .request_with_json_response(Method::PUT, endpoint("/v1/devices/control"), &request)
+            .await?;
+
+        log::trace!("colorTem response: {resp:?}");
+
+        Ok(())
+    }
+
     async fn get_request_with_json_response<T: reqwest::IntoUrl, R: serde::de::DeserializeOwned>(
         &self,
         url: T,
@@ -51,6 +147,28 @@ impl RestApiClient {
             .build()?
             .request(Method::GET, url)
             .header("Govee-API-Key", &self.key)
+            .send()
+            .await?;
+
+        http_response_body(response).await
+    }
+
+    async fn request_with_json_response<
+        T: reqwest::IntoUrl,
+        B: serde::Serialize,
+        R: serde::de::DeserializeOwned,
+    >(
+        &self,
+        method: Method,
+        url: T,
+        body: &B,
+    ) -> anyhow::Result<R> {
+        let response = reqwest::Client::builder()
+            .timeout(Duration::from_secs(60))
+            .build()?
+            .request(method, url)
+            .header("Govee-API-Key", &self.key)
+            .json(body)
             .send()
             .await?;
 
@@ -95,6 +213,9 @@ pub struct RestDeviceInfo {
 pub struct RestDeviceProperties {
     #[serde(rename = "colorTem", default)]
     pub color_temperature: Option<ColorTemperatureProperties>,
+
+    pub mode: Option<JsonValue>,
+    pub gear: Option<JsonValue>,
 }
 
 #[derive(Default, Deserialize, Serialize, Debug, Clone)]
@@ -116,6 +237,8 @@ pub enum SupportedCommand {
     Brightness = "brightness",
     Color = "color",
     ColorTemperature = "colorTem",
+    Mode = "mode",
+    Gear = "gear",
 }
 }
 
@@ -128,6 +251,13 @@ mod test {
     fn list_devices() {
         let resp: GetDevicesResponse =
             from_json(&include_str!("../test-data/rest-list-devices.json")).unwrap();
+        k9::assert_matches_snapshot!(format!("{resp:#?}"));
+    }
+
+    #[test]
+    fn list_appliances() {
+        let resp: GetDevicesResponse =
+            from_json(&include_str!("../test-data/rest-appliances.json")).unwrap();
         k9::assert_matches_snapshot!(format!("{resp:#?}"));
     }
 }
