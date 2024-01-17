55103d0a2ef7b1d70dddcc21806723bdf93c5557
Wez Furlong <wez@wezfurlong.org>
2024-01-17 09:18:39 -0700

    H7131: show gearMode as preset buttons rather than sliders
    refs: https://github.com/wez/govee2mqtt/issues/59

--- src/service/quirks.rs
+++ src/service/quirks.rs
@@ -188,6 +188,7 @@ fn load_quirks() -> HashMap<String, Quirk> {
             .with_platform_temperature_sensor_units(TemperatureUnits::Farenheit),
         Quirk::space_heater("H7131")
             .with_platform_temperature_sensor_units(TemperatureUnits::Farenheit)
+            .with_show_as_preset_modes(&["gearMode"])
             .with_rgb()
             .with_brightness(),
         Quirk::space_heater("H713A")
