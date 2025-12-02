3c67ac8090101cd0a022612f44b8deb33281bf29
Dennis Gaida <2392217+DennisGaida@users.noreply.github.com>
2025-12-02 08:43:52 +0100

    Add docker secret support for handling environment variables
    Refactor environment variable handling to read from file if not present.
    
    Co-authored-by: Wez Furlong <wez@wezfurlong.org>
    
    closes: https://github.com/wez/govee2mqtt/pull/526

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use crate::lan_api::LanDiscoArguments;
 use crate::platform_api::GoveeApiArguments;
 use crate::service::hass::HassArguments;
-use crate::undoc_api::UndocApiArguments;
+use crate::undoc_api::{should_log_sensitive_data, UndocApiArguments};
+use anyhow::Context;
 use clap::Parser;
 use std::str::FromStr;
 
@@ -63,13 +64,45 @@ pub fn opt_env_var<T: FromStr>(name: &str) -> anyhow::Result<Option<T>>
 where
     <T as FromStr>::Err: std::fmt::Display,
 {
+    // Take care: should_log_sensitive_data can recursively call us
+    // with name="GOVEE_LOG_SENSITIVE_DATA".  We only need to
+    // redact values if they are sensitive, and at the time of writing
+    // only variables with PASSWORD in their name match this criteria
+    let log_sensitive_data = !name.contains("PASSWORD") || should_log_sensitive_data();
+
     match std::env::var(name) {
-        Ok(p) => {
-            Ok(Some(p.parse().map_err(|err| {
-                anyhow::anyhow!("parsing ${name}: {err:#}")
-            })?))
+        Ok(p) => Ok(Some(p.parse().map_err(|err| {
+            let mut message = format!("{err:#}");
+            if !log_sensitive_data {
+                message = message.replace(&p, "REDACTED");
+            }
+            anyhow::anyhow!("parsing ${name}: {message}")
+        })?)),
+        Err(std::env::VarError::NotPresent) => {
+            let secret_env_name = format!("{}_FILE", name);
+
+            match std::env::var(&secret_env_name) {
+                Ok(path) => {
+                    let content = std::fs::read_to_string(&path).with_context(|| {
+                        format!(
+                            "Reading secret for {name} from path defined in {secret_env_name}: {path}"
+                        )
+                    })?;
+
+                    let trimmed_content = content.trim_end();
+
+                    Ok(Some(trimmed_content.parse().map_err(|err| {
+                        let mut message = format!("{err:#}");
+                        if !log_sensitive_data {
+                            message = message.replace(trimmed_content, "REDACTED");
+                        }
+                        anyhow::anyhow!("parsing secret content for {name}: {message}")
+                    })?))
+                }
+                Err(std::env::VarError::NotPresent) => Ok(None),
+                Err(err) => anyhow::bail!("${secret_env_name} is invalid: {err:#}"),
+            }
         }
-        Err(std::env::VarError::NotPresent) => Ok(None),
         Err(err) => anyhow::bail!("${name} is invalid: {err:#}"),
     }
 }
