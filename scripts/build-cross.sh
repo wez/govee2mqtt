aa2ff2cd8b3546ff8a36b636f5db9a9f19220fa0
Wez Furlong <wez@wezfurlong.org>
2025-12-13 07:14:49 +0000

    remove deprecated platforms from build
    These are no longer buildable with current versions of the homeassistant
    builder, and have not been supported platforms for homeassistant for > 6
    months.

--- scripts/build-cross.sh
+++ scripts/build-cross.sh
@@ -5,12 +5,6 @@ set -x
 TARGETPLATFORM=$1
 
 case "$TARGETPLATFORM" in
-  "linux/arm/v7")
-    TARGET=armv7-unknown-linux-musleabihf
-    ;;
-  "linux/arm/v6")
-    TARGET=arm-unknown-linux-musleabihf
-    ;;
   "linux/amd64")
     TARGET=x86_64-unknown-linux-musl
     ;;
